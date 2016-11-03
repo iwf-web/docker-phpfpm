0b3a9a47085d64274af0828de5f12b6ac69cd747
Michel Clément <michel@ta-mi.ch>
2016-11-03 19:49:30 +0100

    RZ-222 added time stamp in echo's & added alphabetic sort to execute scripts

--- docker/build/assets/iwfstartup.sh
+++ docker/build/assets/iwfstartup.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 #set -x
 
-if [[ -n ${RUNTIME_ENVIRONMENT} ]]
-  then
+if [[ -n ${RUNTIME_ENVIRONMENT} ]]; then
 
   TARGET_FILE=/app/app/config/parameters.yml
   BASE_LINK_FILE=/data/conf/symfony_configs/parameters_${RUNTIME_ENVIRONMENT}.yml
   DATA_PATH=/data/dockerinit.d/php-fpm
 
-  echo "Preparing for environment ${RUNTIME_ENVIRONMENT}  ..."
+  echo -e "\n------------------------------------------------------------"
+  echo "$(date): Preparing for environment ${RUNTIME_ENVIRONMENT} ..."
 
   # check if file exists, then sym-link it
-  if [ -a ${BASE_LINK_FILE} ]
-    then
+  if [ -a ${BASE_LINK_FILE} ]; then
 
     rm -f ${TARGET_FILE}
     ln -s ${BASE_LINK_FILE} ${TARGET_FILE}
 
-    echo "${TARGET_FILE} is pointing to ${BASE_LINK_FILE} now."
+    echo "$(date): ${TARGET_FILE} is pointing to ${BASE_LINK_FILE} now."
 
-    echo 'Executing startup scripts ...'
+    echo "$(date): Executing startup scripts ..."
 
-    for file in `find "$DATA_PATH" -name "*.sh" -type f`
+    for file in `find "$DATA_PATH" -name "*.sh" -type f | sort -n`
     do
-      echo "Executing script ${file} ..."
+      echo "$(date): Executing script ${file} ..."
       sh ${file}
     done
 
-    echo "Starting $@ ..."
+    echo "$(date): Starting $@ ..."
     exec "$@"
 
   else
 
-    echo "File ${BASE_LINK_FILE} does NOT exist. Aborting."
+    echo "$(date): File ${BASE_LINK_FILE} does NOT exist. Aborting."
     exit 1
 
   fi
 
 else
 
-  echo "Variable RUNTIME_ENVIRONMENT must be set. Aborting."
+  echoline "Variable RUNTIME_ENVIRONMENT must be set. Aborting."
   exit 1
 
 fi
