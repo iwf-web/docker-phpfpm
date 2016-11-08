21ac4dec8b3c04b97bc3ab9ce9fa3d081d70557e
Michel Clément <michel@ta-mi.ch>
2016-11-08 15:07:33 +0100

    Changed postfix configuration for hostname

--- docker/build/assets/iwfpostfix.sh
+++ docker/build/assets/iwfpostfix.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# correct things...
 rm -f /var/lib/postfix/master.lock
 postfix set-permissions
 
+# set runtime settings
+postconf -e myhostname=$HOSTNAME && \
+postconf -F '*/*/chroot = n'
+
+# start postfix and don't end process
 postfix start
 tail -f /var/log/mail.log
