c4cfe4ebb06d3a41f2be973d9cb483f282bfca8a
Jens Hassler <j.hassler@iwf.ch>
2017-06-30 17:03:06 +0200

    PHP 7.1: fixed latest tag

--- docker/build/docker_build.sh
+++ docker/build/docker_build.sh
@@ -67,7 +67,7 @@ if [[ -n ${DEF_BRANCH} ]]; then
   if [ ! -z $PROJECTVERSION ]; then
     BUILD_NUMBER=$PROJECTVERSION
     # As we build for 5.6 base, we set it explicitly
-    DOCKER_LATEST_TAG=7.0-latest
+    DOCKER_LATEST_TAG=7.1-latest
   else
     BUILD_NUMBER=${GIT_BRANCH}-$GIT_COMMIT_SHORT
     DOCKER_LATEST_TAG=${GIT_BRANCH}-latest
