9c24073ce78009ef54bfa3d57728b96c105da270
Jens Hassler <j.hassler@iwf.ch>
2018-09-03 11:09:50 +0200

    RZ-541: add initial folder support to run some scripts only once

--- docker/build/assets/usr/local/bin/iwfstartup.sh
+++ docker/build/assets/usr/local/bin/iwfstartup.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 #set -x
 
-DATA_PATH=/data/dockerinit.d
+DATA_PATH=data/dockerinit.d
+RUN_ONCE_PATH=${DATA_PATH}/initial
+RUN_ONCE_FLAG=${RUN_ONCE_PATH}/initial-run-done
 
 echo -e "\n------------------------------------------------------------"
 echo "$(date): Starting up ..."
 
-echo "$(date): Executing startup scripts ..."
 
-for file in `find "$DATA_PATH" -name "*.sh" -type f | sort -n`
+if [ -d "$RUN_ONCE_PATH" ]; then
+    if [ -f "$RUN_ONCE_FLAG" ]; then
+      echo "$(date): (INITIAL) Flag file found -- not executing files in "initial" folder."
+      echo "$(date): (INITIAL) Please delete the file '${RUN_ONCE_FLAG}' if you want to run the scripts in ${RUN_ONCE_PATH} again.";
+    else
+      echo "$(date): (INITIAL) Executing scripts in 'initial' folder with /bin/sh ..."
+
+      for file in `find "$RUN_ONCE_PATH" -maxdepth 1 -name "*.sh" -type f | sort -n`
+      do
+        echo "$(date): Executing INITIAL script ${file} ..."
+        sh ${file}
+      done
+
+      touch "$RUN_ONCE_FLAG"
+      echo "$(date): (First start) Wrote flag file to prevent running the initial scripts again.";
+
+    fi
+else
+  echo "$(date): (INITIAL) Folder '${RUN_ONCE_PATH}' not found -- please consider adding it and move the scripts that should only be run on the first deployment."
+fi
+
+echo "$(date): Executing startup scripts with /bin/sh ..."
+
+for file in `find "$DATA_PATH" -maxdepth 1 -name "*.sh" -type f | sort -n`
 do
   echo "$(date): Executing script ${file} ..."
   sh ${file}
