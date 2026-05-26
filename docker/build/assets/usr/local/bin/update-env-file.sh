414a96be876329374e99f59f27053bb5d7ac71f0
Jens Hassler <jens.hassler@gmail.com>
2026-05-26 12:45:38 +0200

    fix env variable escaping to support variables with '#' character / clarify env var handling in docs

--- docker/build/assets/usr/local/bin/update-env-file.sh
+++ docker/build/assets/usr/local/bin/update-env-file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printenv | sed -r "s/'/\\\'/gm" | sed -r "s/^([^=]+=)(.*)\$/\1'\2'/gm" > /etc/environment
+printenv | sed -r 's/"/\\"/g' | sed -r 's/^([^=]+=)(.*)$/\1"\2"/' > /etc/environment
